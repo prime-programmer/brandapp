97710cb94a58e0cbeaa458270b918891475a06ac
prime-programmer <chibike.obin@gmail.com>
2022-07-14 10:19:23 +0100

    footer edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,10 +9,10 @@ import {
 
 const Footer = () => {
   return (
-    <div className='bg-black mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
+    <div className='bg-black mx-auto -mt-10 py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
       <div>
         <h1 className='w-full text-3xl font-bold text-[#ad5d28]'>Address:</h1>
-        <p className='py-4'>Lorem Ipsum Drive Saville Road London</p>
+        <p className='py-4'>112 Lorem Ipsum Drive Saville Road London</p>
         <p className='py-4'>LN15 7DZ</p>
         <div className='flex justify-between md:w-[75%] my-6'>
             <FaFacebookSquare size={30} />
